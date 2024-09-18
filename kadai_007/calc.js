90bc14ab9638ea50e0a48087c6a50087ea78b196
ugit5245 <umex.orque2@gmail.com>
2024-09-18 16:29:39 +0900

    IF課題再修正

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,15 +1,15 @@
-let num = 150;
+let num = 16;
 
-if (num % 15 === 0) {
+if (num % 3 === 0 && num % 5 === 0) {
  console.log('3と5の倍数です');
-}
-
-else if (num % 5 === 0) {
- console.log('5の倍数です');
 }
 
 else if (num % 3 === 0) {
  console.log('3の倍数です');
+}
+
+else if (num % 5 === 0) {
+ console.log('5の倍数です');
 }
 
 else {
